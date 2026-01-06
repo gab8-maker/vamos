6fdf9bec56b48df258e37abe64eb4f3b13288311
gab8-maker <gabrielplemos966@gmail.com>
2026-01-06 11:07:56 -0300

    Update maintube.js

--- maintube.js
+++ maintube.js
@@ -1,6 +1,57 @@
 import { execFile } from "child_process"
 import fs from 'fs';
 
+const { spawn } = require("child_process");
+
+/**
+ * Executa um comando Linux genérico
+ */
+function runCommand(cmd, args = []) {
+  return new Promise((resolve, reject) => {
+    const proc = spawn(cmd, args);
+
+    let output = "";
+    let error = "";
+
+    proc.stdout.on("data", data => {
+      output += data.toString();
+    });
+
+    proc.stderr.on("data", data => {
+      error += data.toString();
+    });
+
+    proc.on("close", code => {
+      if (code === 0) {
+        resolve(output.trim());
+      } else {
+        reject(error || `Erro ao executar ${cmd}`);
+      }
+    });
+  });
+}
+
+/**
+ * Retorna o IP público atual
+ */
+async function getPublicIP() {
+  try {
+    const ip = await runCommand("curl", ["-s", "ifconfig.me"]);
+    return ip;
+  } catch (err) {
+    return "Erro ao obter IP";
+  }
+}
+
+/**
+ * Exemplo de uso
+ */
+(async () => {
+  const ip = await getPublicIP();
+  console.log("🌍 IP atual:", ip);
+})();
+
+
 execFile('./yt-dlp', ["-x", "https://www.youtube.com/watch?v=2aSJCWtSIiw", "--audio-format", " mp3", "--audio-quality", " 320k"], async (err, stdout) => {
     if (err) {
         console.error("Erro", err.message);
@@ -27,6 +78,7 @@ const findFile = () => {
         console.log('Arquivos encontrados:', arquivosEncontrados);
     });
 }
+
 
 
 
